731d195df9d8eeb5483260384e60c37fa593b091
Mathieu Lecarme <mathieu@garambrogne.net>
2011-09-22 19:53:53 +0200

    spli spec file.

--- spec/scenario_spec.rb
+++ spec/scenario_spec.rb
@@ -4,27 +4,15 @@ require "minitest/autorun"
 require "scenario"
 
 describe EventMachine::Scenario do
-  it "waits for n actions to be finished" do
-    EM.run do
-      stack = []
-      quorum(5) do |nextStep|
-        5.times do |i|
-          stack << i
-          EM.add_timer(Random.rand(0.1)) do
-            nextStep.call
-          end
-        end
-      end.finally do
-        assert [0, 1, 2, 3, 4] == stack.sort
-        EM.stop
-      end
-    end
+
+  def rand_timer(max, &block)
+    EM::Scenario::Timer.new(Random.rand(max)) { block.call }
   end
 
   it "do the same thing with an iterator" do
     EM.run do
       EM::Iterator.new(0..4, 50).map(proc { |i, iter|
-        EM.add_timer(Random.rand(0.1)) do
+        rand_timer(0.1) do
           iter.return i
         end
       }, proc{ |responses|
@@ -37,109 +25,11 @@ describe EventMachine::Scenario do
   it "iterate with scenario iterator" do
     EM.run do
       EM::Scenario::Iterator.new(0..4) do |i, iter|
-        EM.add_timer(Random.rand(0.1)) do
+        rand_timer(0.1) do
           iter.return i
         end
       end.finally do |responses|
         assert [0, 1, 2, 3, 4] == responses.sort
-        EM.stop
-      end
-    end
-  end
-
-  it "waits for actions and a time out" do
-    EM.run do
-      stack = []
-      q = quorum(20) do |nextStep|
-        20.times do |i|
-          EM.add_timer(Random.rand(0.1)) do
-            stack << i
-            nextStep.call
-          end
-        end
-      end
-      q.errback do |*args|
-        assert [:too_late] == args
-        assert stack.length < 20
-        EM.stop
-      end
-      q.timeout 0.1, :too_late
-      q.finally do
-        assert false, 'too late should happens'
-      end
-    end
-  end
-
-  it "try until success" do
-    EM.run do
-      cpt = 0
-      adnauseum do |nextStep|
-        EM.add_timer(Random.rand(0.1)) do
-          cpt += 1
-          nextStep.call
-        end
-      end.until do
-        cpt > 5
-      end.finally do
-        assert true
-        EM.stop
-      end
-    end
-  end
-
-  it "act 5 times" do
-    EM.run do
-      stack = []
-      adlib(5) do |nextStep, i|
-        stack << i
-        EM.add_timer(Random.rand(0.1)) do
-          nextStep.call
-        end
-      end.finally do
-        assert [0,1,2,3,4] == stack
-        EM.stop
-      end
-    end
-  end
-
-  it "act not so fast" do
-    EM.run do
-      stack = []
-      quantumsatis(5, 2) do |nextStep, i, workers|
-        assert workers <= 2
-        EM.add_timer(Random.rand(0.1)) do
-          stack << i
-          nextStep.call
-        end
-      end.finally do
-        assert (0..4).to_a == stack.sort
-        EM.stop
-      end
-    end
-  end
-
-  it "do something after other thing" do
-    EM.run do
-      txt = ""
-      abinitio do |sequence|
-        sequence.then do |nextStep|
-          EM.add_timer(Random.rand(0.1)) do
-            txt = "Hello "
-            nextStep.call
-          end
-        end.then do |nextStep|
-          EM.add_timer(Random.rand(0.1)) do
-            txt += "World"
-            nextStep.call
-          end
-        end.then do |nextStep|
-          EM.add_timer(Random.rand(0.1)) do
-            txt.upcase!
-            nextStep.call
-          end
-        end
-      end.finally do
-        assert "HELLO WORLD" == txt
         EM.stop
       end
     end
@@ -152,7 +42,7 @@ describe EventMachine::Scenario do
       quorum(2) do |nextStep|
         2.times do |i|
           one << i
-          EM.add_timer(Random.rand(0.1)) do
+          rand_timer(0.1) do
             nextStep.call
           end
         end
@@ -160,7 +50,7 @@ describe EventMachine::Scenario do
         quorum(3) do |nextStep|
           3.times do |i|
             two << i
-            EM.add_timer(Random.rand(0.1)) do
+            rand_timer(0.1) do
               nextStep.call
             end
           end
@@ -171,10 +61,6 @@ describe EventMachine::Scenario do
         end
       end
     end
-  end
-
-  def rand_timer(max, &block)
-    EM::Scenario::Timer.new(Random.rand(max)) { block.call }
   end
 
   it "chain with sequence" do
@@ -242,7 +128,7 @@ describe EventMachine::Scenario do
         m
       end.then do
         rand_timer(0.5) do
-          p stack
+          assert [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] == stack
           EM.stop
         end
       end
